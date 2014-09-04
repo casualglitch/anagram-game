ce38c907fdbd2119c497bfc2379f979e85feb5c4
Lisa Yee <lisayee@live.com>
2014-09-04 00:09:26 -0400

    Hello World simple project

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * CSE 219 Homework 1
+ * @author Lisa Yee 108863013
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
