819c3eaf14a3dc253558ea3ba4fe79325b7b6087
Lisa Yee <lisayee@live.com>
2014-09-04 01:18:08 -0400

    Hello World simple project

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author Lisa Yee 18863013
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
