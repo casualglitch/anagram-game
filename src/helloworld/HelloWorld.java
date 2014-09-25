109309953351a8a20b7e3b92577e404545ab96ce
Lisa Yee <lisayee@live.com>
2014-09-25 15:34:05 -0400

    Relocated files

--- src/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * CSE 219 Homework 1
- * @author Lisa Yee 108863013
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
